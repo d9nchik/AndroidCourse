1cdb42e367f024888fc0f4d058094bf770f30e54
d9nich <54186666+d9nchik@users.noreply.github.com>
2021-11-27 17:45:43 +0200

    merge input and show fragments to main fragment

--- app/src/main/java/com/example/androidcourse/MainFragment.kt
+++ app/src/main/java/com/example/androidcourse/MainFragment.kt
@@ -4,15 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioButton
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LifecycleOwner
 import androidx.navigation.Navigation
+import androidx.room.Room
 import com.example.androidcourse.databinding.FragmentMainBinding
 
 class MainFragment : Fragment(), LifecycleOwner {
-
+    private lateinit var colorNumber: String
+    private lateinit var priceNumber: String
     private lateinit var binding: FragmentMainBinding
+    private var orderDb: OrderDao? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,30 +27,83 @@ class MainFragment : Fragment(), LifecycleOwner {
         binding.recordsButton.setOnClickListener {
             Navigation.findNavController(it).navigate(R.id.action_mainFragment_to_showRecords)
         }
+
+        binding.blueFlowerRadioButton.setOnClickListener { view -> onColorChange(view) }
+        binding.redFlowerRadioButton.setOnClickListener { view -> onColorChange(view) }
+        binding.yellowRadioButton.setOnClickListener { view -> onColorChange(view) }
+
+        binding.minPriceRadioButton.setOnClickListener { view -> onPriceChange(view) }
+        binding.maxPriceValueRadioButton.setOnClickListener { view -> onPriceChange(view) }
+        binding.middlePriceRadioButton.setOnClickListener { view -> onPriceChange(view) }
+        binding.okButton.setOnClickListener { onButtonClick() }
         return binding.root
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        childFragmentManager.setFragmentResultListener(
-            "requestKey",
-            this
-        ) { _, _ -> onButtonClick() }
+
+        orderDb = context?.let {
+            Room.databaseBuilder(
+                it,
+                OrderRoomDatabase::class.java, "order_database"
+            ).build()
+        }?.orderDao()
+
+        colorNumber = getString(R.string.blueFlowerColor)
+        priceNumber = getString(R.string.minPriceValue)
 
     }
 
     private fun onButtonClick() {
-        val inputFragment =
-            childFragmentManager.findFragmentById(R.id.fragment_input_view) as InputFragment
-        val orderDetails = inputFragment.getOrderDetails()
-        if (orderDetails.isNullOrEmpty()) {
+        if (binding.personNameTextField.text.isEmpty()) {
             Toast.makeText(activity, R.string.notAllFieldsFilledPopup, Toast.LENGTH_LONG).show()
             return
         }
-        val showOrderFragment =
-            childFragmentManager.findFragmentById(R.id.fragment_output_view) as ShowOrder
-        showOrderFragment.setMessage(orderDetails)
+
+        Thread {
+            orderDb?.insertOrder(
+                Order(
+                    binding.personNameTextField.text.toString(),
+                    colorNumber,
+                    priceNumber
+                )
+            )
+        }.start()
+
+        binding.textView.text = getString(
+            R.string.order_result,
+            colorNumber,
+            binding.personNameTextField.text,
+            priceNumber
+        )
     }
 
 
+    private fun onColorChange(view: View) {
+        if (view is RadioButton) {
+            // Is the button now checked?
+            if (!view.isChecked) return
+
+            // Check which radio button was clicked
+            colorNumber = when (view.getId()) {
+                R.id.blueFlowerRadioButton -> getString(R.string.blueFlowerColor)
+                R.id.redFlowerRadioButton -> getString(R.string.redFlowerColor)
+                else -> getString(R.string.yellowFlowerColor)
+            }
+        }
+    }
+
+    private fun onPriceChange(view: View) {
+        if (view is RadioButton) {
+            // Is the button now checked?
+            if (!view.isChecked) return
+
+            // Check which radio button was clicked
+            priceNumber = when (view.getId()) {
+                R.id.minPriceRadioButton -> getString(R.string.minPriceValue)
+                R.id.middlePriceRadioButton -> getString(R.string.middlePriceValue)
+                else -> getString(R.string.maxPriceValue)
+            }
+        }
+    }
 }
